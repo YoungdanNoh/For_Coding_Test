44c1f7e76d51148162ee84ededd482886863d5e2
노영단 <38938145+YoungdanNoh@users.noreply.github.com>
2024-03-22 18:05:29 +0900

    [level 3] Title: 여행경로, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.py
+++ 프로그래머스/3/43164. 여행경로/여행경로.py
@@ -1,23 +1,23 @@
 def solution(tickets):
+    answer = []
+    
     graph = dict()
-
-    for (start, end) in tickets:
+    
+    for start, end in tickets:
         if start in graph:
             graph[start].append(end)
         else:
             graph[start] = [end]
     
-    for g in graph.keys():
+    for g in graph:
         graph[g].sort()
     
-    route = []
-    
-    def dfs(start):
+    def dfs(graph, start):
         if start in graph:
             while graph[start]:
-                dfs(graph[start].pop(0))
-        route.append(start)
-
-    dfs("ICN")
+                dfs(graph, graph[start].pop(0))
+        answer.append(start)
+            
+    dfs(graph, "ICN")
     
-    return route[::-1]
+    return answer[::-1]

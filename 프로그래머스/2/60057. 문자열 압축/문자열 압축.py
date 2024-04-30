2c8148fd9220011f503fc2d8939a59c170e40668
노영단 <38938145+YoungdanNoh@users.noreply.github.com>
2024-04-30 21:48:51 +0900

    [level 2] Title: 문자열 압축, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
@@ -0,0 +1,37 @@
+def solution(s):
+    result = 2001
+    
+    for i in range(1, len(s)+1):
+        re_s = ""
+        tmp = s[0:i]
+        start = 0
+        end = i
+        cnt = 1
+        
+        for j in range((len(s)//i)-1):
+            com = s[start+i : end+i]
+            start += i
+            end += i
+            if tmp == com:
+                cnt += 1
+                flag = True
+            else:
+                if cnt == 1:
+                    re_s += tmp
+                else:
+                    re_s += tmp + str(cnt)
+                tmp = com
+                cnt = 1
+                flag = False
+        
+        if cnt == 1:
+            re_s += tmp
+        else:
+            re_s += tmp + str(cnt)
+        if (len(s)%i) != 0:
+            idx = int(len(s)%i)
+            re_s += s[-idx:]
+            
+        result = min(result, len(re_s))
+    
+    return result

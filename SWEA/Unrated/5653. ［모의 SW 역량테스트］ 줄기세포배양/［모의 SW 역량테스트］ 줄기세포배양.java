50d8dbce65a67480ab37cd4bc1ea4a7006ce51cf
노영단 <38938145+YoungdanNoh@users.noreply.github.com>
2024-10-14 20:34:03 +0900

    [Unrated] Title: [모의 SW 역량테스트] 줄기세포배양, Time: 816 ms, Memory: 94,792 KB -BaekjoonHub

--- SWEA/Unrated/5653. ［모의 SW 역량테스트］ 줄기세포배양/［모의 SW 역량테스트］ 줄기세포배양.java
+++ SWEA/Unrated/5653. ［모의 SW 역량테스트］ 줄기세포배양/［모의 SW 역량테스트］ 줄기세포배양.java
@@ -4,102 +4,111 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Solution {
-	static int N,M,K,days;
+	static final int dy[]= {0,0,1,-1};
+	static final int dx[]= {1,-1,0,0};
+
+	static final int DEAD=0, ACTIVE=1, INACTIVE=2;
+	static int T,N,M,K;
+
+	static List<Cell> cell;
+	static PriorityQueue<Cell> pq;
 	static boolean[][] visited;
-	static int[][] map;
-	static int[] dx = {0,1,0,-1};
-	static int[] dy = {1,0,-1,0};
 
-	static Queue<Pos> temp ;
-	static PriorityQueue<Pos> pq;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br=new BufferedReader (new InputStreamReader (System.in));
+		StringTokenizer st=null;
 
-	static class Pos implements Comparable <Pos>{
-		int i,j,originalLife, curLife;
+		T=Integer.parseInt(br.readLine());
 
-		Pos(int i, int j, int originalLife, int curLife) {
-			super();
-			this.i = i;
-			this.j = j;
-			this.originalLife = originalLife;
-			this.curLife = curLife;
-		}
-
-		@Override
-		public int compareTo(Pos o) {
-			// TODO Auto-generated method stub
-			return -Integer.compare(originalLife, o.originalLife);
-		}
-
-	}
-	public static void main(String[] args) throws Exception{
-		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		int t= Integer.parseInt(br.readLine());
-		for(int tc=1 ; tc<=t ; tc++) {
-
-			st = new StringTokenizer(br.readLine(), " ");
-			int r=Integer.parseInt(st.nextToken());
-			int c=Integer.parseInt(st.nextToken());
+		for (int tc=1; tc<=T; tc++) {
+			st=new StringTokenizer (br.readLine());
+			N=Integer.parseInt(st.nextToken());
+			M=Integer.parseInt(st.nextToken());
 			K=Integer.parseInt(st.nextToken());
-			days=0;
-			N = r+K*2+4;
-			M = c+K*2+4;
-			map = new int[N][M];
-			visited =new boolean[N][M];
 
-			pq = new PriorityQueue<>();
-			temp = new LinkedList<Pos>();
+			cell=new ArrayList<>();
+			pq=new PriorityQueue<>( (p1, p2) -> p2.power-p1.power );
+			visited=new boolean[N+2*K][M+2*K];
 
-			for(int i=N/2-1 ; i<N/2-1+r ; i++) {
-				st = new StringTokenizer(br.readLine()," ");
-				for(int j=M/2-1 ; j<M/2-1+c ; j++) {
-					int num=Integer.parseInt(st.nextToken());
-					if(num!=0) {
-						map[i][j]=num;
-						pq.offer(new Pos(i,j,num,num*2));
-						visited[i][j]=true;
 
+			for (int i=0; i<N; i++) {
+				st=new StringTokenizer (br.readLine());
+				for (int j=0; j<M; j++) {
+					int n=Integer.parseInt(st.nextToken());
+
+					if (n!=0) {
+						cell.add(new Cell(i+K,j+K,n,n));
+						visited[i+K][j+K]=true;
 					}
+
 				}
 			}
+			simulation();
+			System.out.println("#"+tc+" "+count());
 
-			bfs();
-
-			System.out.println("#"+tc+" "+pq.size());
 		}
-
 	}
 
-	private static void bfs() {
-		// TODO Auto-generated method stub
-		for(int t=1 ; t<=K ; t++) {
-			while(!pq.isEmpty()) {
-				Pos cur = pq.poll();
+	static void simulation () {
+		for (int k=1; k<=K; k++) {
 
-				cur.curLife=cur.curLife-1;
-				// 원래 curlife를 2배로 잡아서 og가 커지면 그때부터 활성화
-				if(cur.originalLife>cur.curLife) {
-					for(int i=0; i<4; i++) {
-						int nx= cur.i + dx[i];
-						int ny= cur.j + dy[i];
-						if(0>nx || nx>N || 0>ny || ny> M) continue;
+			// 직전에 INACTIVE -> ACTIVE 상태로 변경된 cell들
+			while (!pq.isEmpty()) {
+				Cell c=pq.poll();
+				int y=c.y;
+				int x=c.x;
 
-						if(visited[nx][ny]==false) {
-							visited[nx][ny]=true;
-							//시간별로 나누기 위해 temp넣어두고 pq 다돌면 (1시간이 지나) temp에 있는거 다시 pq 넣어주
-							temp.offer(new Pos(nx,ny,cur.originalLife, cur.originalLife*2));
-						}
-					}
+				if (!visited[y][x]) {
+					visited[y][x]=true;
+					cell.add(c);
 				}
-				// 죽지 않은 경우
-				if(cur.curLife != 0) temp.offer(new Pos(cur.i,cur.j,cur.originalLife,cur.curLife));
 			}
 
-			while(!temp.isEmpty()) {
-				pq.offer(temp.poll());
+			for (int i=0; i<cell.size(); i++) {
+				if (cell.get(i).state==DEAD) continue;
+
+				else if (cell.get(i).state==INACTIVE && cell.get(i).time==k) {
+					cell.get(i).state=ACTIVE;				// X시간동안 활성상태
+					cell.get(i).time=k+cell.get(i).power;	// 현재 시간 보다 X 시간이 지난 후에 죽는 상태로 만들어줄 것
+
+
+					for (int d=0; d<4; d++) {
+						// k+1+power 후에 변경상태가 될 것
+						int ny=cell.get(i).y+dy[d];
+						int nx=cell.get(i).x+dx[d];
+
+						pq.add(new Cell(ny,nx, k+1+cell.get(i).power, cell.get(i).power));
+					}
+
+				} else if (cell.get(i).state==ACTIVE && cell.get(i).time==k) {
+					cell.get(i).state=DEAD;
+					cell.get(i).time=0;
+					cell.get(i).power=0;
+				}
+
 			}
 		}
+	}
 
+	static int count () {
+		int cnt=0;
+		for (int i=0; i<cell.size(); i++) {
+			if (cell.get(i).state==ACTIVE || cell.get(i).state==INACTIVE)
+				cnt++;
+		}
+		return cnt;
+	}
+
+
+	static class Cell {
+		int y,x,time,state,power;
+
+		Cell (int y, int x, int time, int power) {
+			this.y=y;
+			this.x=x;
+			this.time=time;
+			this.power=power;
+			this.state=INACTIVE;
+		}
 	}
 }

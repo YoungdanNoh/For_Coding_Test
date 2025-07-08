a76b0a0d0cf5b8852fbbdfdab29a2440c8b1586f
노영단 <shdbwjd705270@gmail.com>
2025-07-08 11:19:33 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 508 ms, Memory: 56860 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -3,76 +3,71 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-class Edge implements Comparable<Edge> {
-
-    int end, weight;
-
-    public Edge(int end, int weight) {
-        this.end = end;
-        this.weight = weight;
-    }
-
-    @Override
-    public int compareTo(Edge o) {
-        return this.weight - o.weight;
-    }
-}
-
 public class Main {
 
-    static List<Edge>[] graph;
-    static int total;
+    static int V;
+    static int E;
+    static ArrayList<Node>[] graph;
+
+    static class Node implements Comparable<Node> {
+        int node;
+        int weight;
+        
+        public Node(int node, int weight){
+            this.node = node;
+            this.weight = weight;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return this.weight - o.weight;
+        }
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
         st = new StringTokenizer(bf.readLine());
-        int V = Integer.parseInt(st.nextToken()); // 정점의 개수
-        int E = Integer.parseInt(st.nextToken()); // 간선의 개수
-
+        V = Integer.parseInt(st.nextToken());
+        E = Integer.parseInt(st.nextToken());
+        
         graph = new ArrayList[V+1];
-
-        for (int i = 0; i <= V; i++) {
-            graph[i] = new ArrayList<Edge>();
+        for(int i = 0; i <= V; i++){
+            graph[i] = new ArrayList<Node>();
         }
 
-        for (int i = 1; i <= E; i++) {
+        for(int i=0; i<E; i++) {
             st = new StringTokenizer(bf.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            int weight = Integer.parseInt(st.nextToken());
-            // 방향이 없으므로 두 방향으로 모두 추가해준다.
-            graph[start].add(new Edge(end, weight));
-            graph[end].add(new Edge(start, weight));
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            
+            graph[a].add(new Node(b, c));
+            graph[b].add(new Node(a, c));
         }
 
-        prim(1, V);
-        System.out.println(total);
-    }
+        boolean[] visited = new boolean[V+1];
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.offer(new Node(1, 0));
+        
+        int cost = 0;
+        while(!pq.isEmpty()) {
+            Node node = pq.poll();
+            
+            if(visited[node.node]) continue;
 
-    public static void prim(int start, int v) {
-
-        boolean[] visited = new boolean[v+1];
-
-        PriorityQueue<Edge> pq = new PriorityQueue<>();
-        pq.add(new Edge(v, 0)); //시작 노드 넣기
-
-        while (!pq.isEmpty()) {
-            Edge e = pq.poll();
-            int next = e.end;
-            int cost = e.weight;
-
-            if (visited[e.end]) continue;
-
-            visited[next] = true;
-            total += cost;
-
-            for (Edge edge : graph[next]) {
-                if (!visited[edge.end]){
-                    pq.add(new Edge(edge.end, edge.weight));
+            visited[node.node] = true;
+            cost += node.weight;
+            
+            for(Node n : graph[node.node]) {
+                // 나와 연결된 애들
+                if(!visited[n.node]) {
+                    pq.offer(new Node(n.node, n.weight));
                 }
             }
         }
+
+        System.out.println(cost);
     }
 }

a0eb85306a1ed64938c8ed51157dcd1a2f4b2ea4
노영단 <shdbwjd705270@gmail.com>
2025-07-09 11:41:48 +0900

    [Gold IV] Title: 네트워크 연결, Time: 508 ms, Memory: 49680 KB -BaekjoonHub

--- 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
@@ -1,88 +1,89 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.PriorityQueue;
+import java.util.Arrays;
 import java.util.StringTokenizer;
-
-class Edge implements Comparable<Edge> {
-
-    int next;
-    int weight;
-
-    public Edge(int next, int weight) {
-        this.next = next;
-        this.weight = weight;
-    }
-
-    @Override
-    public int compareTo(Edge o) {
-        return this.weight - o.weight; //오름차순
-    }
-}
 
 public class Main {
 
-    static List<Edge>[] graph;
-    static int cost = 0;
+    static int N;
+    static int M;
+    static Edge[] e;
+    static int[] parent;
+
+    static class Edge implements Comparable<Edge> {
+        int from;
+        int to;
+        int cost;
+
+        public Edge(int from, int to, int cost) {
+            this.from = from;
+            this.to = to;
+            this.cost = cost;
+        }
+
+        @Override
+        public int compareTo(Edge o) {
+            return this.cost - o.cost;
+        }
+    }
+
+    public static int find(int x){
+        if(parent[x] == x) return x;
+        return parent[x] = find(parent[x]);
+    }
+
+    public static boolean union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+
+        if(rootX == rootY) return false;
+
+        if(rootX < rootY){
+            parent[rootX] = rootY;
+
+        }else{
+            parent[rootY] = rootX;
+        }
+
+        return true;
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        int N = Integer.parseInt(bf.readLine().trim());
-        int M = Integer.parseInt(bf.readLine().trim());
-
-        graph = new ArrayList[N + 1];
-
-        for (int i = 0; i <= N; i++) {
-            graph[i] = new ArrayList<>();
+        st = new StringTokenizer(bf.readLine());
+        N = Integer.parseInt(st.nextToken());
+        parent = new int[N+1];
+        for (int i = 0; i < N+1; i++) {
+            parent[i] = i;
         }
 
-        for (int i = 1; i <= M; i++) {
-            st = new StringTokenizer(bf.readLine().trim());
-            int u = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
-            int weight = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(bf.readLine());
+        M = Integer.parseInt(st.nextToken());
+        e = new Edge[M];
 
-            //방향이 없으므로 양방향으로 넣어준다.
-            graph[u].add(new Edge(v, weight));
-            graph[v].add(new Edge(u, weight));
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(bf.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            e[i] = new Edge(a, b, c);
         }
 
-        prim(1, N); // 시작 노드, 전체 노드 개수
-        System.out.println(cost);
-    }
+        Arrays.sort(e);
 
-    public static void prim(int u, int N) {
-        boolean[] visited = new boolean[N + 1];
+        int result = 0;
 
-        PriorityQueue<Edge> pq = new PriorityQueue<>();
-        pq.add(new Edge(u, 0)); //시작 노드의 비용은 0
-
-        while (!pq.isEmpty()) {
-            Edge e = pq.poll();
-            int next = e.next;
-            int weight = e.weight;
-
-            if(visited[next]) {
-                continue; // 방문한 노드라면 pass
-            }
-
-            visited[next] = true;
-            cost += weight;
-            //System.out.println(weight);
-            //System.out.println(cost);
-
-            for(Edge edge: graph[next]) {
-                // 다음 노드와 연결된 노드들을 탐색
-                if(!visited[edge.next]) {
-                    // 방문하지 않은 노드라면 pq에 삽입
-                    //System.out.println(weight);
-                    pq.add(new Edge(edge.next, edge.weight));
-                }
+        for (int i = 0; i < M; i++) {
+            if(union(e[i].from, e[i].to)) {
+                // 연결 가능
+                result += e[i].cost;
             }
         }
+
+        System.out.println(result);
     }
 }

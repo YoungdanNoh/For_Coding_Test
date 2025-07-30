ba1558aae6a5c4c56c072dc0d05412c2f573a64f
노영단 <shdbwjd705270@gmail.com>
2025-07-30 10:57:09 +0900

    [level 2] Title: 후보키, Time: 39.94 ms, Memory: 68.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42890. 후보키/후보키.java
@@ -0,0 +1,110 @@
+import java.util.*;
+
+class Solution {
+    
+    static int N;
+    static int M;
+    static boolean[] visited;
+    static boolean[] isSelected2;
+    static boolean[] isSelected;
+    static String[][] table;
+    static int result = 0;
+    static boolean flag;
+    static ArrayList<String> candi = new ArrayList<>();
+    
+    public void check(int cnt){
+        if(cnt == M){
+            
+            String tmp = "";
+            for(int i=0; i<M; i++){
+                
+                if(isSelected2[i]){
+                        tmp += i;
+                }
+            }
+            
+            for(int i=0; i<candi.size(); i++){
+                if(candi.get(i).equals(tmp)){
+                    flag = true;
+                }
+            }
+            
+            return;
+        }
+        
+        // 현재 선택된 후보 집합이라면..
+        if(isSelected[cnt]){
+            isSelected2[cnt] = false;
+            check(cnt+1);
+            isSelected2[cnt] = true;
+            check(cnt+1);
+            
+        }else{
+            check(cnt + 1);
+        }
+        
+    }
+    
+    public void subSet(int cnt){
+        if(cnt == M){
+            Set<String> s = new HashSet<>();
+            
+            for(int i=0; i<N; i++){
+                // 현재 부분집합인 애들로 후보키 구성해보기
+                String tmp = "";
+                for(int j=0; j<M; j++){
+                    if(isSelected[j]){
+                        tmp += table[i][j];
+                    }
+                }
+                if(!tmp.equals("")){
+                    s.add(tmp);
+                }
+                
+            }
+            
+            if(s.size() == N){
+                // 현재 선택된 애들로 부분집합 구성 시 후보키가 되는지 체크
+                isSelected2 = new boolean[M];
+                check(0);
+                
+                if(flag){
+                    // 최소성 만족 X
+                    flag = false;
+                    return;
+                }
+                
+                String tmp2 = "";
+                for(int i=0; i<M; i++){
+                    if(isSelected[i]){
+                        tmp2 += i;
+                        visited[i] = true;
+                    }
+                }
+                candi.add(tmp2);
+                result++;
+            }
+            
+            return;
+        }
+        
+        isSelected[cnt] = false;
+        subSet(cnt+1);
+        isSelected[cnt] = true;
+        subSet(cnt+1);
+        
+    }
+    
+    public int solution(String[][] relation) {
+        N = relation.length;
+        M = relation[0].length;
+        visited = new boolean[M];
+        isSelected = new boolean[M];
+        
+        table = relation;
+        
+        subSet(0);
+        
+        return result;
+    }
+}

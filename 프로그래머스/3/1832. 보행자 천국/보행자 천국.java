9224b82e70f3beaba6a8db18c49e4af887180122
노영단 <shdbwjd705270@gmail.com>
2025-08-12 10:38:07 +0900

    [level 3] Title: 보행자 천국, Time: 78.76 ms, Memory: 111 MB -BaekjoonHub

--- 프로그래머스/3/1832. 보행자 천국/보행자 천국.java
+++ 프로그래머스/3/1832. 보행자 천국/보행자 천국.java
@@ -12,30 +12,15 @@ class Solution {
                 
                 if (i == 0 && j == 0) continue;
                 
-                if(cityMap[i][j] == 0){
-                    // 자유 통행
-                    
-                    // 왼쪽에서 들어오는 값
-                    if (j > 0) { 
-                        right[i][j] = (cityMap[i][j-1] == 2)
-                            ? (right[i][j-1] % MOD) // 직진만 가능
-                            : ((right[i][j-1] + down[i][j-1]) % MOD); // 자유 통행이면 합
-                    }
-                    
-                    // 위쪽에서 들어오는 값
-                    if (i > 0) {
-                        down[i][j] = (cityMap[i-1][j] == 2)
-                            ? (down[i-1][j] % MOD) // 직진만 가능
-                            : ((right[i-1][j] + down[i-1][j]) % MOD);
-                    }
-                
-                }else if(cityMap[i][j] == 1){
+                if(cityMap[i][j] == 1){
                     // 통행 금지
                     right[i][j] = 0;
                     down[i][j] = 0;
                 
                 }else{
-                    // 왼쪽에서 들어오는 값
+                    // 0또는 2인 경우(자유통행 또는 터널)
+                    
+                    //왼쪽에서 들어오는 값
                     if (j > 0) { 
                         right[i][j] = (cityMap[i][j-1] == 2)
                             ? (right[i][j-1] % MOD) // 직진만 가능

6187f0faa558f7019af28c4cb8e7a2176cd5dba6
노영단 <shdbwjd705270@gmail.com>
2025-08-19 10:18:50 +0900

    [level 4] Title: 호텔 방 배정, Time: 412.77 ms, Memory: 166 MB -BaekjoonHub

---
+++ 프로그래머스/4/64063. 호텔 방 배정/호텔 방 배정.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Solution {
+    
+    // x가 이미 배정된 경우, 다음으로 시도할 방 번호를 가리킴
+    Map<Long, Long> next = new HashMap<>();
+    
+    // x 이상에서의 가장 작은 빈 방을 찾음
+    public long find(long x) {
+        
+        if (!next.containsKey(x)) {
+            // x방이 비어있으면 해당 방을 줌
+            // x방 요청이 왔을 때 줄 수 있는 방 번호는 x+1번부터 시작
+            next.put(x, x + 1);
+            return x;
+        }
+        
+        // x방을 배정받을 수 없다면 next[x]로 점프해서 빈 방 찾기
+        long empty = find(next.get(x));
+        
+        // 경로 압축: 다음에 x요청이 오면 그 다음 빈 방 탐색
+        next.put(x, empty);
+        return empty;
+    }
+    
+    public long[] solution(long k, long[] room_number) {
+        long[] answer = new long[room_number.length];
+        
+        for (int i = 0; i < room_number.length; i++) {
+            long r = find(room_number[i]);
+            
+            answer[i] = r;
+        }
+        
+        return answer;
+    }
+}

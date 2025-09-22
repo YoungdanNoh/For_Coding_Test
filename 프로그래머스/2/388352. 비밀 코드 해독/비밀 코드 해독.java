375a8045870c60e0a9ef0f5b65c4979738795658
노영단 <shdbwjd705270@gmail.com>
2025-09-22 10:44:19 +0900

    [level 2] Title: 비밀 코드 해독, Time: 234.07 ms, Memory: 119 MB -BaekjoonHub

--- 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.java
+++ 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.java
@@ -3,55 +3,62 @@ import java.util.*;
 class Solution {
     
     static int N;
-    static int[] choice = new int[5];
     static int[][] Q;
     static int[] Ans;
     static int result = 0;
-    
-    public void subSet(int cnt, int last){
-        if(cnt == 5){
-            // 일치도 체크
-            HashMap<Integer, Integer> m = new HashMap<>();
-            for(int i=0; i<5; i++){
-                m.put(choice[i], 1);
-            }
-            
-            int[] match = new int[Ans.length];
-            for(int i=0; i<Q.length; i++){
-                int tmp = 0;
-                
-                for(int j=0; j<Q[i].length; j++){
-                    if(m.get(Q[i][j]) != null){
-                        tmp++;
-                    }
-                }
-                match[i] = tmp;
-            }
-            
-            boolean flag = true;
-            for(int i=0; i<Ans.length; i++){
-                if(match[i] != Ans[i]){
-                    flag = false;
-                    break;
-                }
-            }
-            if(flag) result++;
-            
-            return;
-        }
-        
-        for(int i=last+1; i<=N; i++){
-            choice[cnt] = i;
-            subSet(cnt+1, i);
-        }
-    }
+    static int[] subset;
     
     public int solution(int n, int[][] q, int[] ans) {
         N = n;
         Q = q;
         Ans = ans;
-        subSet(0, 0);
+        subset = new int[5];
+        
+        // 1 ~ n까지 5개 부분집합 구하기
+        subSet(1, 0);
         
         return result;
+    }
+    
+    public void subSet(int start, int cnt){
+        if(cnt == 5){
+            
+            // 각 q에 대해 subset과 몇개가 일치하는지 매칭하기
+            Map<Integer, Integer> m = new HashMap<>();
+            for(int i=0; i<5; i++){
+                m.put(subset[i], 1);
+            }
+            
+            int[] tmp = new int[Q.length];
+            for(int i=0; i<Q.length; i++){
+                int check = 0;
+                
+                for(int j=0; j<Q[i].length; j++){
+                    if(m.get(Q[i][j]) != null){
+                        check++;
+                    }
+                }
+                
+                tmp[i] = check;
+            }
+            
+            boolean flag = true;
+            for(int i=0; i<Ans.length; i++){
+                if(Ans[i] != tmp[i]){
+                    flag = false;
+                    break;
+                }
+            }
+            
+            if(flag) result++;
+            
+            return;
+        }
+        
+        for(int i=start; i<=N; i++){
+            subset[cnt] = i;
+            subSet(i+1, cnt+1);
+        }
+        
     }
 }

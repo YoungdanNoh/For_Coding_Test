681920f7a7ecc9cc486e2389b8764b68dce3c396
노영단 <shdbwjd705270@gmail.com>
2025-10-30 09:56:19 +0900

    [Gold II] Title: 로봇 조종하기, Time: 1312 ms, Memory: 72804 KB -BaekjoonHub

--- 백준/Gold/2169. 로봇 조종하기/로봇 조종하기.py
+++ 백준/Gold/2169. 로봇 조종하기/로봇 조종하기.py
@@ -1,36 +1,41 @@
-n, m = map(int, input().split())
+import sys
 
-dp = []
-for _ in range(n):
-    dp.append(list(map(int, input().split())))
+read = sys.stdin.readline
 
-for i in range(1, m):
-    # 1행에 관해서는 오른쪽으로만 이동 가능
-    dp[0][i] += dp[0][i-1]
+N, M = map(int, read().split())
 
-for x in range(1, n):
-    right = dp[x][:]
-    left = dp[x][:]
+board = []
+for _ in range(N):
+    tmp = list(map(int, read().split()))
+    board.append(tmp)
 
-    for y in range(m):
-        # 오른쪽으로 이동할 때
-        if y == 0:
-            # 1열 이라면 위에서 내려오는 경우밖에 없음
-            right[y] += dp[x-1][y]
-        else:
-            # 위에서 내려오는 경우 and 왼쪽 값을 더하는 경우 중 큰 값을 더한다
-            right[y] += max(dp[x-1][y], right[y-1])
+# 첫 행은 왼쪽에서 오른쪽으로 가는 방법밖에 없음
+for i in range(1):
+    for j in range(1, M):
+        board[i][j] += board[i][j-1]
 
-    for y in range(m-1, -1, -1):
-        # 왼쪽으로 이동할 때
-        if y == m-1:
-            # 가장 마지막 열이라면 위에서 내려오는 경우밖에 없다.
-            left[y] += dp[x-1][y]
-        else:
-            # 위에서 내려오는 경우 and 오른쪽 값을 더하는 경우 중 큰 값을 더한다
-            left[y] += max(dp[x-1][y], left[y+1])
+for i in range(1, N):
+    # 행별로 위쪽에서 내려오는 경우
+    up = [0 for _ in range(M)]
+    for j in range(M):
+        up[j] = board[i-1][j]
 
-    for y in range(m):
-        dp[x][y] = max(left[y], right[y])
+    # 왼 -> 오 경우들의 가치
+    left = [0 for _ in range(M)]
+    left[0] = up[0] + board[i][0]
+    for j in range(1, M):
+        # 왼쪽에서 오는 경우, 위에서 내려오는 경우 중 최대값을 찾기
+        left[j] = max(left[j - 1], up[j]) + board[i][j]
 
-print(dp[n-1][m-1])
+    # 오 -> 왼 경우들의 가치
+    right = [0 for _ in range(M)]
+    right[M-1] = up[M-1] + board[i][M-1]
+    for j in range(M - 2, -1, -1):
+        # 오른쪽에서 오는 경우, 위에서 내려오는 경우 중 최대값을 찾기
+        right[j] = max(right[j + 1], up[j]) + board[i][j]
+
+    # 왼 -> 오, 오 -> 왼 중 최대 가치로 현재 위치를 갱신
+    for j in range(M):
+        board[i][j] = max(left[j], right[j])
+
+print(board[N-1][M-1])

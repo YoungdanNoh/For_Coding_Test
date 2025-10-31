80b1682fae943103b93418a221169bb51b2f31a8
노영단 <shdbwjd705270@gmail.com>
2025-10-31 10:56:36 +0900

    [level 3] Title: 가장 먼 노드, Time: 63.66 ms, Memory: 20.1 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -2,46 +2,45 @@ import heapq
 
 def solution(n, edge):
     
+    pq = []
     graph = [[] for _ in range(n+1)]
+    dist = [50001 for _ in range(n+1)]
+    visited = [False for _ in range(n+1)]
     
     for a, b in edge:
         graph[a].append(b)
         graph[b].append(a)
     
-    dist = [1e9]*(n+1)
-    visited = [False]*(n+1)
-    q = []
-    
-    heapq.heappush(q, (0, 1))
+    # 시작 노드의 거리는 0
     dist[1] = 0
     
-    while q:
-        cd, cn = heapq.heappop(q)
+    heapq.heappush(pq, (0, 1)) # 출발지 넣기
+    
+    while pq:
+        cost, node = heapq.heappop(pq)
         
-        if visited[cn]:
+        if visited[node]:
             continue
         
-        visited[cn] = True
+        visited[node] = True
         
-        for nn in graph[cn]:
-            # cn과 연결된 노드 탐색
-            if dist[nn] > (cd + 1):
-                dist[nn] = cd + 1
-                heapq.heappush(q, (dist[nn], nn))
+        # 현재 노드와 연결된 노드들 탐색
+        for nn in graph[node]:
+            if visited[nn]:
+                continue
+            if dist[nn] > dist[node] + 1:
+                dist[nn] = dist[node] + 1
+                heapq.heappush(pq, (dist[nn], nn))
+        
+    dist.pop(0)
     
-    dist.sort(reverse = True)
-    mdist = -1
-    cnt = 0
-    
-    for d in dist:
-        if d == 1e9:
-            continue    
-        elif mdist == -1:
-            mdist = d
-            
-        if d == mdist and d != 0:
-            cnt += 1
-        elif mdist != -1:
-            break
-    
-    return cnt
+    mdist = 0
+    ans = 0
+    for i in range(len(dist)):
+        if mdist < dist[i] and dist[i] != 50001:
+            mdist = dist[i]
+            ans = 1
+        elif mdist == dist[i] and mdist != 0:
+            ans += 1
+        
+    return ans

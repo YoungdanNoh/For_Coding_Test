9db7579a8bc89a4f8f97da208c730e72ba94ac37
노영단 <shdbwjd705270@gmail.com>
2026-02-02 15:43:58 +0900

    [level 3] Title: 등대, Time: 139.50 ms, Memory: 44.1 MB -BaekjoonHub

--- 프로그래머스/3/133500. 등대/등대.py
+++ 프로그래머스/3/133500. 등대/등대.py
@@ -14,25 +14,26 @@ def solution(n, lighthouse):
     def dfs(cur, parent):
         global ans
         
-        # 1. 리프 노드인 경우
+        # 1. cur이 리프 노드인 경우
         # 연결된 노드가 부모 한 개뿐이면 리프임
         if len(graph[cur]) == 1 and graph[cur][0] == parent:
             # 리프는 키지 않고 부모가 키는 것이 더 최적값(부모는 여러 자식을 가질 수 있기 때문)
-            # 1은 키지 않겠다는 의미
+            
+            # cur은 키지 않겠다
             return 1
         
-        # 2. 리프가 아닌 경우: 자식들 dfs
-        tmp_res = 0
+        # 2. cur이 리프가 아닌 경우: 자식 노드들의 dfs 결과를 수집한다
+        tmp = 0
         for nxt in graph[cur]:
             if nxt == parent:
                 # 부모로 되돌아 가는 것을 방지
                 continue
-            tmp_res += dfs(nxt, cur)
+            tmp += dfs(nxt, cur)
             
         # 3. 현재 노드를 켤지 말지 결정
         # 자식들이 모두 0을 반환했다면(= 모두 자식들이 켜져 있음)
         # 현재 노드는 안 켜도 됨
-        if tmp_res == 0:
+        if tmp == 0:
             # 부모는 키지 않음
             return 1
         

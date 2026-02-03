7710ed5c3297f9b2cbe10ba1d55b6a5437dce683
노영단 <shdbwjd705270@gmail.com>
2026-02-03 16:40:11 +0900

    [Gold I] Title: 구슬 탈출, Time: 64 ms, Memory: 35068 KB -BaekjoonHub

--- 백준/Gold/13459. 구슬 탈출/구슬 탈출.py
+++ 백준/Gold/13459. 구슬 탈출/구슬 탈출.py
@@ -1,86 +1,92 @@
 import sys
 from collections import deque
 
-read = sys.stdin.readline
+input = sys.stdin.readline
 
-N, M = map(int, read().split())
-board = []
+N, M = map(int, input().split(" "))
+board = [0]*N
 
-rx, ry, bx, by = 0, 0, 0, 0
+rx, ry = 0, 0 # 빨간 공 위치
+bx, by = 0, 0 # 파란 공 위치
 for i in range(N):
-    tmp = list(read().strip())
-    board.append(tmp)
+    board[i] = (list(input().strip()))
 
-    for j in range(len(tmp)):
-        if tmp[j] == 'R':
+    for j in range(M):
+        if board[i][j] == 'R':
             rx, ry = i, j
-        elif tmp[j] == 'B':
+        elif board[i][j] == 'B':
             bx, by = i, j
 
-def move(x, y, dx, dy):
-    cnt = 0
+
+def move(i, j, x, y):
+    cnt = 0 # 이동 횟수
 
     while True:
-        tx = x + dx
-        ty = y + dy
+        tx = x + i
+        ty = y + j
 
+        if tx<0 or tx>=N or ty<0 or ty>=M:
+            break
         if board[tx][ty] == '#':
-            # 벽에 도달
-            return x, y, cnt, False
-        elif board[tx][ty] == 'O':
-            # 구멍에 도달
-            return tx, ty, cnt+1, True
-
-        x, y = tx, ty
-        cnt += 1
-
-dir = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-visited = set()
-visited.add((rx, ry, bx, by))
-q = deque()
-q.append((rx, ry, bx, by, 0)) # 빨간 구슬 위치, 파란 구슬 위치, 이동 횟수
-
-ans = 0
-flag = False
-while q:
-    crx, cry, cbx, cby, cnt = q.popleft()
-
-    if cnt >= 10:
-        # 이동 횟수가 10번을 넘어감
-        continue
-
-    for dx, dy in dir:
-        # 각 구슬을 해당 방향으로 끝까지 이동시킴
-        # 빨간 구슬 이동
-        nrx, nry, rcnt, rgoal = move(crx, cry, dx, dy)
-        # 파란 구슬 이동
-        nbx, nby, bcnt, bgoal = move(cbx, cby, dx, dy)
-
-        if bgoal:
-            # 파란 구슬이 빠지면 실패!
-            continue
-
-        if rgoal:
-            ans = 1
-            flag = True
+            # 벽
+            break
+        if board[tx][ty] == 'O':
+            # 구멍
+            cnt += 1
+            x, y = tx, ty
             break
 
-        if nrx == nbx and nry == nby:
-            # 둘의 위치가 같다면 이동 횟수로 더 앞에 있는 구슬을 판단한다
+        cnt += 1
+        x, y = tx, ty
+
+    return cnt, x, y
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+# dfs 상하좌우 움직이기
+q = deque()
+q.append((0, rx, ry, bx, by))
+visited = set()
+visited.add((rx, ry, bx, by))
+
+ans = 0
+while q:
+    cur = q.popleft()
+
+    if cur[0] >= 10:
+        # 10번 움직였다면
+        continue
+
+    for i in range(4):
+        rcnt, rx, ry = move(dx[i], dy[i], cur[1], cur[2]) # red 사방으로 이동시켜보기
+        bcnt, bx, by = move(dx[i], dy[i], cur[3], cur[4]) # blue 사방으로 이동시켜보기
+
+        if board[bx][by] == 'O':
+            # 파란 구슬이 구멍에 빠졌으므로 실패
+            continue
+
+        if board[rx][ry] == 'O':
+            # 성공
+            ans = 1
+            break
+
+        if rx == bx and ry == by:
+            # 둘이 같은 위치에 있다면
             if rcnt > bcnt:
-                # 빨간 구슬이 더 많이 움직였으므로 파란 구슬보다 뒤에 있음
-                nrx -= dx
-                nry -= dy
+                # 빨간공이 더 뒤
+                rx = rx - dx[i]
+                ry = ry - dy[i]
             else:
-                nbx -= dx
-                nby -= dy
+                bx = bx - dx[i]
+                by = by - dy[i]
 
-        loc = (nrx, nry, nbx, nby)
-        if loc not in visited:
-            visited.add(loc)
-            q.append((nrx, nry, nbx, nby, cnt+1))
+        v = (rx, ry, bx, by)
+        if v not in visited:
+            visited.add(v)
+            q.append((cur[0]+1, rx, ry, bx, by))
 
-    if flag:
+    if ans == 1:
         break
 
 print(ans)

5a25681b7c87f452aa62d4d35091b86d3af0530b
노영단 <shdbwjd705270@gmail.com>
2026-02-05 11:20:10 +0900

    [level 3] Title: 등산코스 정하기, Time: 544.72 ms, Memory: 88 MB -BaekjoonHub

--- 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.py
+++ 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.py
@@ -1,52 +1,51 @@
 import heapq
 
 def solution(n, paths, gates, summits):
-    # 각 게이트에서 출발 -> 산봉우리까지만 이동
-    graph = [[] for _ in range(n+1)]
+    # 각 게이트에서 출발 -> 산봉우리까지 이동
+    # 되돌아올 때는 동일한 경로로 돌아오면 됨
+    
+    graph = [[] for i in range(n+1)]
     
     for a, b, c in paths:
         graph[a].append((b, c))
         graph[b].append((a, c))
         
+    INF = 10**15
+    dist = [INF for i in range(n+1)]
+    visited = [False for i in range(n+1)]
+    
     g_set = set(gates)
     s_set = set(summits)
     
-    INF = 10**15
-    q = []
-    visited = [False]*(n+1)
-    dist = [INF]*(n+1)
-    
+    pq = []
     for g in gates:
-        # 모든 게이트에서부터 시작하기
-        heapq.heappush(q, (0, g)) # (intensity, 번호)
+        heapq.heappush(pq, (0, g)) # intensity, 시작점
         dist[g] = 0
+    
+    while pq:
+        inten, node = heapq.heappop(pq)
         
-    while q:
-        cost, node = heapq.heappop(q)
-        
+        if node in s_set:
+            # 산봉우리 도착
+            continue
+            
         if visited[node]:
             continue
         visited[node] = True
         
-        # 산봉우리에 도착했다면 더 이상 탐색하지 않기
-        if node in s_set:
-            continue
+        for nn, cost in graph[node]:
             
-        for nn, c in graph[node]:
-            # 현재 노드와 연결된 다른 노드들 탐색
             if nn in g_set:
-                # 다른 게이트로는 이동하지 않는다
+                # 다른 게이트로는 이동x
                 continue
             
-            # 현재 경로까지의 intensity와 다음 노드까지의 intensity를 비교해서 더 큰 값을 찾음
-            next_inten = max(cost, c)
-            if dist[nn] > next_inten:
-                # 현재 노드를 거쳐가는 것이 더 최소값이라면
-                dist[nn] = next_inten
-                heapq.heappush(q, (dist[nn], nn))
+            new_inten = max(inten, cost) # 해당 경로에서의 intensity를 계산
+                
+            if dist[nn] > new_inten:
+                dist[nn] = new_inten
+                heapq.heappush(pq, (dist[nn], nn))
     
-
-    # 산봉우리까지의 dist를 보고 그 값이 가장 최소가 되는 곳을 정답으로 고름
+    # 산봉우리 노드들을 순차적으로 순회하며 ans값 탐색
     summits.sort()
     min_inten = INF
     ans = [0, 0]

c8966b721f6030642bc20944237f3e8aebd3a888
노영단 <shdbwjd705270@gmail.com>
2026-02-11 16:35:22 +0900

    [Gold I] Title: 열쇠, Time: 212 ms, Memory: 35068 KB -BaekjoonHub

--- 백준/Gold/9328. 열쇠/열쇠.py
+++ 백준/Gold/9328. 열쇠/열쇠.py
@@ -1,36 +1,33 @@
+# 빌딩 안팎으로 이동 가능
+# 따라서 전체 맵을 .으로 감싸서 어느 곳에서든 움직일 수 있도록 만들어준다.
 import sys
 from collections import deque
 
 input = sys.stdin.readline
-
 T = int(input())
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-for _ in range(T):
-    ans = 0
+for t in range(T):
+    h, w = map(int, input().strip().split())
 
-    h, w = map(int, input().split())
-
-    board = [['.' for i in range(w+2)]]
-
+    graph = [['.' for _ in range(w+2)]]
     for i in range(h):
         tmp = list(input().strip())
-        board.append(['.']+tmp+['.'])
-    board.append(['.' for i in range(w+2)])
+        graph.append(['.'] + tmp + ['.'])
+    graph.append(['.' for _ in range(w+2)])
 
-    key = list(input().strip())
+    keys = set(input().strip())
 
-    # (0, 0)에서 bfs 시작
+    # (0, 0)에서 부터 bfs 시작
+    visited = [[False]*(w+2) for _ in range(h+2)]
     q = deque()
-    visited = [[False]*(w+2) for i in range(h+2)]
-
     q.append((0, 0))
     visited[0][0] = True
 
     waiting = []
-
+    ans = 0
     while q:
         cur = q.popleft()
 
@@ -43,50 +40,44 @@ for _ in range(T):
             if visited[tx][ty]:
                 continue
 
-            if board[tx][ty] == '*':
-                # 벽이라면 건너뛰기
-                continue
-            elif board[tx][ty] == '.':
+            if graph[tx][ty] == '.':
                 # 빈 공간
-                visited[tx][ty] = True
                 q.append((tx, ty))
-            elif board[tx][ty] == '$':
-                # 문서가 있는 곳
+                visited[tx][ty] = True
+            elif graph[tx][ty] == '*':
+                # 벽
+                continue
+            elif graph[tx][ty] == '$':
+                # 문서
                 ans += 1
-                visited[tx][ty] = True
-                board[tx][ty] = '.'
                 q.append((tx, ty))
-            elif ord(board[tx][ty]) < 97:
-                # 문이 있는 곳
-                ap = ord(board[tx][ty]) + 32 # 키의 아스키 코드 값
-
-                if chr(ap) in key:
-                    # 해당 키를 가지고 있다면 문 열 수 있음
-                    visited[tx][ty] = True
-                    board[tx][ty] = '.'
+                graph[tx][ty] = '.'
+                visited[tx][ty] = True
+            elif 'A' <= graph[tx][ty] <= 'Z':
+                # 문
+                if chr(ord(graph[tx][ty]) + 32) in keys:
+                    # 키를 가지고 있다면 방문 가능
                     q.append((tx, ty))
+                    graph[tx][ty] = '.'
+                    visited[tx][ty] = True
                 else:
-                    # 해당 위치는 따로 담아두어 대기
+                    # 다음에 키를 획득할 수 있으므로 일단 저장
                     waiting.append((tx, ty))
             else:
-                # 키를 획득
-                key.append(board[tx][ty])
-                board[tx][ty] = '.'
+                # 키
+                keys.add(graph[tx][ty])
+                graph[tx][ty] = '.'
                 visited[tx][ty] = True
                 q.append((tx, ty))
 
-                remove_list = []
-                for x, y in waiting:
-                    ap = ord(board[x][y]) + 32
-
-                    if chr(ap) in key:
-                        # 해당 키를 가지고 있다면 문 열 수 있음
-                        visited[x][y] = True
-                        board[x][y] = '.'
+                for wait in range(len(waiting)-1, -1, -1):
+                    x = waiting[wait][0]
+                    y = waiting[wait][1]
+                    if chr(ord(graph[x][y]) + 32) in keys:
+                        # 키를 가지고 있다면 방문 가능
                         q.append((x, y))
-                        remove_list.append((x, y))
-
-                for r in remove_list:
-                    waiting.remove(r)
+                        graph[x][y] = '.'
+                        visited[x][y] = True
+                        waiting.pop(wait) # 해당 장소를 웨이팅 리스트에서 삭제
 
     print(ans)

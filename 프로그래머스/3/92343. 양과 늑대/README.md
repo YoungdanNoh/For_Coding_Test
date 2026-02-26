d0de091d23ec43e6289f6ac9966ded7931e9942e
노영단 <shdbwjd705270@gmail.com>
2026-02-26 17:37:21 +0900

    [level 3] Title: 양과 늑대, Time: 11.36 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/README.md
+++ 프로그래머스/3/92343. 양과 늑대/README.md
@@ -1,10 +1,10 @@
 # [level 3] 양과 늑대 - 92343 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92343) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92343?language=python3) 
 
 ### 성능 요약
 
-메모리: 79.3 MB, 시간: 5.80 ms
+메모리: 9.23 MB, 시간: 11.36 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 22일 11:10:10
+2026년 02월 26일 17:37:25
 
 ### 문제 설명
 
--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.py
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.py
@@ -1,27 +1,27 @@
 def solution(info, edges):
+    ans = [] # 정답이 될 수 있는 것들의 집합
     
-    visited = [False]*len(info)
-    result = []
+    visited = [False]*(len(info)) # 방문처리
+    visited[0] = True # 0번 노드부터 시작
     
     def dfs(sheep, wolf):
-        if(sheep <= wolf):
+        if sheep <= wolf:
             return
         else:
-            result.append(sheep)
+            ans.append(sheep)
             
-        for parent, child in edges:
-            if(visited[parent] and not visited[child]):
-                # 부모는 방문했지만, 자식을 방문하지 않은 경우 해당 자식을 방문 or 안 함
-                visited[child] = True
+        for p, c in edges:
+            # 부모 방문, 자식 미방문인 경우 -> 자식 노드를 방문하거나 안 하거나
+            if visited[p] and not visited[c]:
+                visited[c] = True # 방문하는 경우에 대해 끝까지 탐색 시작
                 
-                if(info[child] == 0):
+                if info[c] == 0:
                     dfs(sheep+1, wolf)
                 else:
                     dfs(sheep, wolf+1)
                 
-                visited[child] = False
-    
-    visited[0] = True
+                visited[c] = False # 방문하지 않는 경우로 바꾸어 다음 경우의 수들을 탐색 시작
+                
     dfs(1, 0)
     
-    return max(result)
+    return max(ans)

eb8534b64e9c8cb1d583d6f66ac1c5b9cfcbe935
노영단 <shdbwjd705270@gmail.com>
2026-03-04 16:53:25 +0900

    [level 4] Title: 취소되지 않은 진료 예약 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
@@ -0,0 +1,12 @@
+with register_patient(APNT_YMD, APNT_NO, PT_NO, MCDP_CD, MDDR_ID, APNT_CNCL_YN, APNT_CNCL_YMD) as(
+    select *
+    from APPOINTMENT
+    where APNT_CNCL_YN = 'N'
+    and DATE_FORMAT(APNT_YMD, '%Y-%m-%d') = '2022-04-13'
+    and MCDP_CD = 'CS'
+)
+SELECT rp.APNT_NO, p.PT_NAME, p.PT_NO, d.MCDP_CD, d.DR_NAME, rp.APNT_YMD
+from PATIENT as p, DOCTOR as d, register_patient as rp
+where p.PT_NO = rp.PT_NO
+and d.DR_ID = rp.MDDR_ID
+order by APNT_YMD;

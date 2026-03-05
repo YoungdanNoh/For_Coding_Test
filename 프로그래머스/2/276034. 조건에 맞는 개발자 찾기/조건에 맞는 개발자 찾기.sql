e8fb2d306fc767738704c8bf1f251145592dc2d2
노영단 <shdbwjd705270@gmail.com>
2026-03-05 19:10:48 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -1,5 +1,5 @@
 select distinct ID, EMAIL, FIRST_NAME, LAST_NAME
-from SKILLCODES as A, DEVELOPERS as B
-where SKILL_CODE & A.CODE = A.CODE
-and (NAME = 'Python' or NAME = 'C#')
+from SKILLCODES, DEVELOPERS
+where NAME IN ('Python', 'C#')
+and SKILL_CODE & SKILLCODES.CODE = SKILLCODES.CODE
 order by ID

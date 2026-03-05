2c3819729734624a7514d2094477d32d0a374f6b
노영단 <shdbwjd705270@gmail.com>
2026-03-05 12:10:38 +0900

    [level 2] Title: 업그레이드 된 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
+++ 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
@@ -1,7 +1,8 @@
 select ITEM_ID, ITEM_NAME, RARITY
 from ITEM_INFO
 where ITEM_ID IN (select b.ITEM_ID
-                 from ITEM_INFO a, ITEM_TREE b
-                 where a.ITEM_ID = b.PARENT_ITEM_ID
-                 and RARITY = 'RARE')
+                from ITEM_INFO a, ITEM_TREE b
+                where a.ITEM_ID = b.PARENT_ITEM_ID
+                and RARITY = 'RARE'
+                )
 order by ITEM_ID desc

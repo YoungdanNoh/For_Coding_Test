f6f1a2ec6abfe6d964f9aa908477e212f39231e9
노영단 <shdbwjd705270@gmail.com>
2026-05-11 17:13:14 +0900

    [level 4] Title: 호텔 방 배정, Time: 308.87 ms, Memory: 54.3 MB -BaekjoonHub

--- 프로그래머스/4/64063. 호텔 방 배정/호텔 방 배정.py
+++ 프로그래머스/4/64063. 호텔 방 배정/호텔 방 배정.py
@@ -6,18 +6,19 @@ sys.setrecursionlimit(100000)
 next = {}
 def find(require):
     if require not in next:
-        # 해당 방 번호에 배정된 사항 없으므로 해당 방을 배정
-        next[require] = require + 1 # 해당 방 번호를 또 요청한다면 이후로는 require+1번 방부터 배정 가능
+        # 고객이 요청하는 방이 비어있음
+        next[require] = require+1
         return require
     
-    empty = find(next[require]) # 재귀를 돌면서 빈 방 찾기
-    next[require] = empty + 1
-    return empty
+    # 고객이 요청하는 방을 줄 수 없음
+    possible = find(next[require]) # 다음 빈 방 찾기
+    next[require] = possible+1
+    return possible
     
-
+    
 def solution(k, room_number):
     ans = [0 for _ in range(len(room_number))]
     for i in range(len(room_number)):
-        ans[i] = find(room_number[i])
+        ans[i] = find(room_number[i]) # 각 요청 별 방 번호 구하기
     
     return ans

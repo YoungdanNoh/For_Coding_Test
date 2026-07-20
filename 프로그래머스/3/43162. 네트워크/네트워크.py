90fcc332b29875316ab6656a2f3e3c566bbe1178
노영단 <shdbwjd705270@gmail.com>
2026-07-20 17:47:35 +0900

    [level 3] Title: 네트워크, Time: 0.48 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,17 +1,40 @@
 def solution(n, computers):
-    visited = [False] * n
-                
-    result = 0
-    for i in range(n):
-        if visited[i] == False:
-            dfs(computers, i, visited, n)
-            result += 1
-        
-    return result
-
-def dfs(computers, i, visited, n):
-    visited[i] = True
     
-    for j in range(n):
-        if visited[j] == False and j != i and computers[j][i] == 1:
-            dfs(computers, j, visited, n)
+    parent = [0]*n
+    for i in range(n):
+        parent[i] = i
+        
+    def find(x):
+        if parent[x] == x:
+            return x
+        
+        parent[x] = find(parent[x])
+        return parent[x]
+    
+    def union(x, y):
+        a = find(x)
+        b = find(y)
+        
+        if a == b:
+            return False
+        
+        if a < b:
+            parent[b] = a
+        else:
+            parent[a] = b
+        return True
+    
+    for i in range(n):
+        for j in range(n):
+            if i == j:
+                continue
+                
+            if computers[i][j] == 1:
+                # 두 컴퓨터를 잇는다
+                union(i, j)
+    
+    # 모든 컴퓨터에 대해 find를 수행하여 최상위 루트를 가르키도록 한다.
+    for i in range(n):
+        find(i)
+                
+    return len(set(parent))
